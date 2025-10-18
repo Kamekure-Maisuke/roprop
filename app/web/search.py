650d590d165e28d96fa4e7519039979f4d380c00
taisuke ohno <taisukeohno@taisukenoMacBook-Pro.local>
2025-10-18 12:49:39 +0900

    fix format

--- app/web/search.py
+++ app/web/search.py
@@ -19,41 +19,48 @@ async def view_search(request: Request, q: str = "") -> Template:
 
             # PC検索
             for pc in await P.raw(
-                "SELECT * FROM pcs WHERE ARRAY[name, model, serial_number] &@~ {}", query
+                "SELECT * FROM pcs WHERE ARRAY[name, model, serial_number] &@~ {}",
+                query,
             ):
-                cached_results.append({
-                    "type": "pc",
-                    "id": str(pc["id"]),
-                    "title": f"PC: {pc['name']}",
-                    "description": f"{pc['model']} ({pc['serial_number']})",
-                    "link": f"/pcs/{pc['id']}/show",
-                })
+                cached_results.append(
+                    {
+                        "type": "pc",
+                        "id": str(pc["id"]),
+                        "title": f"PC: {pc['name']}",
+                        "description": f"{pc['model']} ({pc['serial_number']})",
+                        "link": f"/pcs/{pc['id']}/show",
+                    }
+                )
 
             # 社員検索
             for emp in await E.raw(
                 "SELECT * FROM employees WHERE ARRAY[name, email] &@~ {}", query
             ):
-                cached_results.append({
-                    "type": "employee",
-                    "id": str(emp["id"]),
-                    "title": f"社員: {emp['name']}",
-                    "description": emp["email"],
-                    "link": f"/employees/{emp['id']}/show",
-                })
+                cached_results.append(
+                    {
+                        "type": "employee",
+                        "id": str(emp["id"]),
+                        "title": f"社員: {emp['name']}",
+                        "description": emp["email"],
+                        "link": f"/employees/{emp['id']}/show",
+                    }
+                )
 
             # ブログ検索
             for blog in await B.raw(
                 "SELECT * FROM blog_posts WHERE ARRAY[title, content] &@~ {}", query
             ):
-                cached_results.append({
-                    "type": "blog",
-                    "id": str(blog["id"]),
-                    "title": f"ブログ: {blog['title']}",
-                    "description": blog["content"][:100] + "..."
-                    if len(blog["content"]) > 100
-                    else blog["content"],
-                    "link": f"/blogs/{blog['id']}/detail",
-                })
+                cached_results.append(
+                    {
+                        "type": "blog",
+                        "id": str(blog["id"]),
+                        "title": f"ブログ: {blog['title']}",
+                        "description": blog["content"][:100] + "..."
+                        if len(blog["content"]) > 100
+                        else blog["content"],
+                        "link": f"/blogs/{blog['id']}/detail",
+                    }
+                )
 
             search_cache[cache_key] = cached_results
 
